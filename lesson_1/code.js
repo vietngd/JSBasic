fa58b9ed88d45160e102a5169370e29a66be390a
vietngd <viet.ngd.2002@gmail.com>
2023-07-10 23:34:19 +0700

    new object

---
+++ lesson_1/code.js
@@ -0,0 +1,35 @@
+// number
+var a = 1;
+var b = 2;
+console.log(typeof a);
+// string
+var hoTen = "Nguyen Viet";
+console.log(hoTen);
+// if - else
+var built = null;
+if (built) {
+  console.log("DIEU KIEN DUNG");
+} else {
+  console.log("Dieu kien sai");
+}
+// boolean
+var isSuccess = a > b;
+console.log(isSuccess);
+// undefined
+var lient;
+console.log(lient);
+// null
+var lient = null;
+console.log(lient);
+// symbol
+var id1 = Symbol("id");
+var id2 = Symbol("id");
+console.log(id1, id2);
+
+var myObject1 = {
+  name: "Viet",
+  age: 21,
+};
+console.log("log", myObject1);
+var myArray1 = ["Book", "Null", "Dog", "Cat"];
+console.log("log2", myArray1);
--- lesson_1/data.js
+++ lesson_1/data.js
@@ -12,23 +12,37 @@
  - Object
  */
 // Number type
-var a = 1;
-var b = 2;
-var c = 1.5;
+// var a = 1;
+// var b = 2;
+// var c = 1.5;
 // String type
-var fullName = "Viet Nguyen Duc";
-console.log(fullName);
-// Boolean type
-var isSuccess = true;
-// Underfined type : khai bao bien nhung khong gan gia tri
-var age;
-// Null type
-var inNull = null; // null: khong co gi
-// Symbol type
-var id = symbol("id"); // unique
+// var fullName = "Viet Nguyen Duc";
+// console.log(fullName);
+// // Boolean type
+// var isSuccess = true;
+// // Underfined type : khai bao bien nhung khong gan gia tri
+// var age;
+// // Null type
+// var inNull = null; // null: khong co gi
+// // Symbol type
+// var id = symbol("id"); // unique
 
 // Funcion
-var myFunction = funcion(){
-    alert('Hi, Xin chao cac ban');
-}
-myFunction();
+// var myFunction = funcion() {
+//     alert("Hi, Xin chao cac ban");
+// }
+// myFunction();
+// Object type
+var myObject = {
+  name: "Nguyen Duc Viet",
+  age: 18,
+  address: "Bac Ninh",
+  myFunction: function () {},
+};
+console.log("my Object", myObject);
+
+var myArray = ["Javascipt", "PHP", "Ruby"];
+console.log(myArray);
+/**
+ * muon kiem tra kieu du lieeu thi dung console.log(typeof <ten bien>)
+ */
